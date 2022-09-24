9f7fdb6d10e2cfa73190b871c5ae2c17795db476
CraftinPark <craftinpark@gmail.com>
2022-09-23 23:31:15 -0700

    editable members

--- src/components/MembersPanel.tsx
+++ src/components/MembersPanel.tsx
@@ -1,6 +1,6 @@
 import React, { useState, Dispatch, SetStateAction } from "react";
 import { Box, Typography, Paper, TextField, Select, MenuItem, Button, IconButton } from "@mui/material";
-import { Delete, Add } from "@mui/icons-material";
+import { Delete, Add, Person, CheckBox, Clear, CheckBoxOutlineBlank } from "@mui/icons-material";
 import { v4 as uuidv4 } from "uuid";
 import { sex, member } from "../util/types";
 import { tableCell, tableFormCell, tableHeaderCell } from "./styles";
@@ -31,30 +31,59 @@ const MembersPanel = ({ members, setMembers }: MembersPanelProps) => {
       setMembers(mems);
    }
 
+   function editMember(index: number, property: string, update: any) {
+      setMembers((current) => {
+         let newMembers = [...current];
+         switch (property) {
+            case "name":
+               newMembers[index].name = update;
+               break;
+            case "secondaryName":
+               newMembers[index].secondaryName = update;
+               break;
+            case "sex":
+               newMembers[index].sex = update;
+               break;
+            case "year":
+               newMembers[index].year = update;
+               break;
+            case "leader":
+               newMembers[index].leader = update;
+               break;
+         }
+
+         return newMembers;
+      });
+   }
+
    function renderMembers(): JSX.Element {
       return (
          <Box sx={{ mt: 1, mb: 2 }}>
             <Box sx={{ display: "flex" }}>
-               <Box sx={tableHeaderCell} width="24.5%">
+               <Box sx={tableHeaderCell} width="6%"></Box>
+               <Box sx={tableHeaderCell} width="23.5%">
                   <Typography>Name</Typography>
                </Box>
-               <Box sx={tableHeaderCell} width="24.5%">
+               <Box sx={tableHeaderCell} width="23.5%">
                   <Typography>Alt name</Typography>
                </Box>
-               <Box sx={tableHeaderCell} width="15%">
+               <Box sx={tableHeaderCell} width="14%">
                   <Typography>Sex</Typography>
                </Box>
-               <Box sx={tableHeaderCell} width="15%">
+               <Box sx={tableHeaderCell} width="14%">
                   <Typography>Year</Typography>
                </Box>
-               <Box sx={tableHeaderCell} width="15%">
+               <Box sx={tableHeaderCell} width="14%">
                   <Typography>Leader</Typography>
                </Box>
-               <Box sx={tableHeaderCell} width="6%"></Box>
+               <Box sx={tableHeaderCell} width="5%"></Box>
             </Box>
             {members.map((member, index) => (
-               <Box key={member.id} sx={{ display: "flex", backgroundColor: member.active ? "lightgreen" : "white" }}>
-                  <Box sx={tableCell} width="24.5%">
+               <Box
+                  key={member.id}
+                  sx={{ display: "flex", height: "37px", backgroundColor: member.active ? "lightgreen" : "white" }}
+               >
+                  <Box sx={tableCell} width="6%">
                      <button
                         style={{
                            background: "none",
@@ -77,45 +106,96 @@ const MembersPanel = ({ members, setMembers }: MembersPanelProps) => {
                            });
                         }}
                      >
-                        <Typography>{member.name}</Typography>
+                        {member.active ? <CheckBox style={{ color: "green" }} /> : <CheckBoxOutlineBlank />}
                      </button>
                   </Box>
-                  <Box sx={tableCell} width="24.5%">
-                     <button
-                        style={{
-                           background: "none",
-                           color: "inherit",
-                           border: "none",
-                           padding: 0,
-                           font: "inherit",
-                           cursor: "pointer",
-                           outline: "none",
-                           width: "100%",
-                           height: "100%",
-                        }}
-                        onClick={() => {
-                           setMembers((prev: member[]) => {
-                              const updateMember = prev[index];
-                              updateMember.active = !updateMember.active;
-                              const newMembers = [...prev];
-                              newMembers[index] = updateMember;
-                              return newMembers;
-                           });
-                        }}
+                  <Box sx={tableCell} width="23.5%">
+                     <TextField
+                        inputProps={{ style: { textAlign: "center" } }}
+                        required
+                        size="small"
+                        value={member.name}
+                        onChange={(e: any) => editMember(index, "name", e.target.value)}
+                     />
+                  </Box>
+                  <Box sx={tableCell} width="23.5%">
+                     <TextField
+                        inputProps={{ style: { textAlign: "center" } }}
+                        required
+                        size="small"
+                        value={member.secondaryName}
+                        onChange={(e: any) => editMember(index, "secondaryName", e.target.value)}
+                     />
+                  </Box>
+                  <Box sx={tableCell} width="14%">
+                     <Select
+                        SelectDisplayProps={{ style: { color: member.sex === "male" ? "blue" : "deeppink" } }}
+                        fullWidth
+                        size="small"
+                        value={member.sex}
+                        onChange={(e: any) => editMember(index, "sex", e.target.value)}
                      >
-                        <Typography>{member.secondaryName}</Typography>
-                     </button>
+                        <MenuItem value={"male"}>M</MenuItem>
+                        <MenuItem value={"female"}>F</MenuItem>
+                     </Select>
                   </Box>
-                  <Box sx={tableCell} width="15%">
-                     <Typography>{member.sex}</Typography>
+                  <Box sx={tableCell} width="14%">
+                     <TextField
+                        inputProps={{
+                           style: {
+                              backgroundColor: "transparent",
+                              outline: "none",
+                              border: "none",
+                              height: "100%",
+                              fontSize: "16px",
+                              textAlign: "center",
+                           },
+                        }}
+                        required
+                        placeholder="Year"
+                        type="number"
+                        size="small"
+                        value={member.year}
+                        onChange={(e: any) => editMember(index, "year", e.target.value)}
+                     />
                   </Box>
-                  <Box sx={tableCell} width="15%">
-                     <Typography>{member.year}</Typography>
+                  <Box sx={tableCell} width="14%">
+                     <Select
+                        inputProps={{
+                           style: {
+                              backgroundColor: "transparent",
+                              outline: "none",
+                              border: "none",
+                              WebkitAppearance: "none",
+                              width: "100%",
+                              height: "100%",
+                              fontSize: "18px",
+                              textAlign: "center",
+                           },
+                        }}
+                        fullWidth
+                        size="small"
+                        value={member.leader.toString()}
+                        onChange={(e: any) =>
+                           e.target.value === "true"
+                              ? editMember(index, "leader", true)
+                              : editMember(index, "leader", false)
+                        }
+                     >
+                        <MenuItem value={"true"} style={{ justifyContent: "center" }}>
+                           <div style={{ display: "flex", alignItems: "center" }}>
+                              <Person fontSize="small" style={{ color: "green" }} />
+                           </div>
+                        </MenuItem>
+
+                        <MenuItem value={"false"} style={{ justifyContent: "center" }}>
+                           <div style={{ display: "flex", alignItems: "center" }}>
+                              <Clear style={{ opacity: "20%" }} fontSize="small" />
+                           </div>
+                        </MenuItem>
+                     </Select>
                   </Box>
-                  <Box sx={tableCell} width="15%">
-                     <Typography>{member.leader ? "true" : "false"}</Typography>
-                  </Box>
-                  <Box sx={{ ...tableCell, backgroundColor: "gainsboro" }} width="6%">
+                  <Box sx={{ ...tableCell, backgroundColor: "gainsboro" }} width="5%">
                      <IconButton onClick={() => removeMember(index)}>
                         <Delete />
                      </IconButton>
@@ -129,31 +209,34 @@ const MembersPanel = ({ members, setMembers }: MembersPanelProps) => {
    function newMemberForm(): JSX.Element {
       return (
          <Box sx={{ display: "flex" }}>
-            <Box sx={tableFormCell} width="24.5%">
+            <Box sx={tableFormCell} width="6%"></Box>
+            <Box sx={tableFormCell} width="23.5%">
                <TextField
                   required
                   placeholder="Name"
                   size="small"
                   value={name}
+                  fullWidth
                   onChange={(e: any) => setName(e.target.value)}
                />
             </Box>
-            <Box sx={tableFormCell} width="24.5%">
+            <Box sx={tableFormCell} width="23.5%">
                <TextField
                   required
                   placeholder="Alt name"
                   size="small"
                   value={secondaryName}
+                  fullWidth
                   onChange={(e: any) => setSecondaryName(e.target.value)}
                />
             </Box>
-            <Box sx={tableFormCell} width="15%">
+            <Box sx={tableFormCell} width="14%">
                <Select fullWidth size="small" value={sex} onChange={(e: any) => setSex(e.target.value)}>
-                  <MenuItem value={"male"}>male</MenuItem>
-                  <MenuItem value={"female"}>female</MenuItem>
+                  <MenuItem value={"male"}>M</MenuItem>
+                  <MenuItem value={"female"}>F</MenuItem>
                </Select>
             </Box>
-            <Box sx={tableFormCell} width="15%">
+            <Box sx={tableFormCell} width="14%">
                <TextField
                   required
                   placeholder="Year"
@@ -163,7 +246,7 @@ const MembersPanel = ({ members, setMembers }: MembersPanelProps) => {
                   onChange={(e: any) => setYear(e.target.value)}
                />
             </Box>
-            <Box sx={tableFormCell} width="15%">
+            <Box sx={tableFormCell} width="14%">
                <Select
                   fullWidth
                   size="small"
@@ -174,7 +257,7 @@ const MembersPanel = ({ members, setMembers }: MembersPanelProps) => {
                   <MenuItem value={"false"}>false</MenuItem>
                </Select>
             </Box>
-            <Box sx={tableFormCell} width="6%">
+            <Box sx={tableFormCell} width="5%">
                <Button
                   style={{ maxWidth: "100%", minWidth: "100%", maxHeight: "100%", minHeight: "100%" }}
                   variant="contained"
